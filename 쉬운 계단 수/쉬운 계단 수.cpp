e8e6f1cfc3cf0aecccee133f0b77cfa8f712a5d2
P513 <psb9663@gmail.com>
2021-05-05 23:24:40 +0900

    solve 쉬운 계단 수

---
+++ 쉬운 계단 수/쉬운 계단 수.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int dp[101][10];
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == 0) {
+				dp[i][j] = dp[i - 1][j + 1];
+			}
+			else if (j == 9) {
+				dp[i][j] = dp[i - 1][j - 1];
+			}
+			else {
+				dp[i][j] = dp[i - 1][j + 1] + dp[i - 1][j - 1];
+			}
+			dp[i][j] %= 1000000000;
+		}
+	}
+	int sum = 0;
+	for (int i = 0; i < 10; i++) {
+		sum += dp[n][i];
+		sum %= 1000000000;
+	}
+	printf("%d", sum);
+	return 0;
+}
