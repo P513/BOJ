136bff2e585a8e263d9bc27e4767de9a7a398e7d
P513 <psb9663@gmail.com>
2022-02-10 22:40:09 +0900

    modify 미로 탐색

--- 미로 탐색/미로 탐색.cpp
+++ 미로 탐색/미로 탐색.cpp
@@ -1,46 +1,52 @@
-#include <iostream>
-#include <tuple>
-#include <vector>
+#include <stdio.h>
 #include <queue>
+#include <utility>
 using namespace std;
-int visited[101][101], map[101][101], ans, n, m, cnt;
-int dx[4] = { 1,-1,0,0 }, dy[4] = { 0,0,1,-1 };
-queue<tuple<int, int, int>>q;
-
-int check(int x, int y) {
-	if (x < 0 || y < 0 || x >= n || y >= m)return 0;
-	if (map[x][y] == 0)return 0;
-	if (visited[x][y] == 1)return 0;
-	return 1;
-}
-void bfs(int x, int y, int cnt) {
-	if (x == n - 1 && y == m - 1) { ans = cnt + 1; return; }
-	for (int i = 0; i < 4; i++) {
-		int next_x = x + dx[i], next_y = y + dy[i];
-		if (check(next_x, next_y)) {
-			visited[next_x][next_y] = 1;
-			q.push(make_tuple(next_x, next_y, cnt + 1));
+queue<pair<int, int>>q;
+int maze[101][101], cnt[101][101];
+int main()
+{
+	int x, y;
+	scanf("%d %d", &x, &y);
+	q.push(make_pair(1, 1));
+	cnt[1][1] = 1;
+	for (int i = 1; i <= x; i++)
+	{
+		for (int j = 1; j <= y; j++)
+		{
+			scanf("%1d", &maze[i][j]);
 		}
 	}
-	while (!q.empty()) {
-		int next_x = get<0>(q.front());
-		int next_y = get<1>(q.front());
-		int cnt = get<2>(q.front());
+	while (!q.empty())
+	{
+		int a = q.front().first;
+		int b = q.front().second;
 		q.pop();
-		bfs(next_x, next_y, cnt);
-	}
-	return;
-}
-int main() {
-	scanf("%d %d", &n, &m);
-	// 1이 이동 가능
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf("%1d", &map[i][j]);
+		if (maze[a + 1][b] == 1 && a + 1 <= x && cnt[a + 1][b] == 0)
+		{
+			cnt[a + 1][b] = cnt[a][b] + 1;
+			q.push(make_pair(a + 1, b));
+		}
+		if (maze[a - 1][b] == 1 && a - 1 > 0 && cnt[a - 1][b] == 0)
+		{
+			cnt[a - 1][b] = cnt[a][b] + 1;
+			q.push(make_pair(a - 1, b));
+		}
+		if (maze[a][b + 1] == 1 && b + 1 <= y && cnt[a][b + 1] == 0)
+		{
+			cnt[a][b + 1] = cnt[a][b] + 1;
+			q.push(make_pair(a, b + 1));
+		}
+		if (maze[a][b - 1] == 1 && b - 1 > 0 && cnt[a][b - 1] == 0)
+		{
+			cnt[a][b - 1] = cnt[a][b] + 1;
+			q.push(make_pair(a, b - 1));
+		}
+		if (a == x && b == y)
+		{
+			printf("%d", cnt[x][y]);
+			break;
 		}
 	}
-	visited[0][0] = 1;
-	bfs(0, 0, 0);
-	printf("%d", ans);
 	return 0;
 }
